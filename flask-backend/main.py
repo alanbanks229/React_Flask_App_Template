0f7c2d65d2998a817fd25b65fb8e34639cefba9f
alanbanks229 <alanbanks229@gmail.com>
2020-06-29 21:40:22 -0400

    I did it, flask and react can communicate with eachother

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,4 +1,9 @@
 import flask
+"""
+How can I send a list from python to react? 
+convert it to json, then add it with 
+`<script> list = {{ my_django_list_json }} </script>`
+"""
 
 app = flask.Flask("__main__")
 
@@ -6,5 +11,9 @@ app = flask.Flask("__main__")
 
 def my_index():
     return flask.render_template("index.html", token="Helo Flask React World")
+
+@app.route("/sup")
+def sup():
+    return flask.render_template("index.html", token="On the Sup Page, not really still index.html")
 
 app.run(debug=True)
